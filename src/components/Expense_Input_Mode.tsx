97fd7fa0e883965b7538d6be19454066b58f4091
drypuddle1220 <steventaehyunkim@gmail.com>
2024-10-01 20:40:45 -0400

    update v_1
    changed about.html and worked on the component of expense_input_mode

---
+++ src/components/Expense_Input_Mode.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+export default function Expense_Input_Mode() {
+
+    const Expense_Input_Model = ({ isOpen, onClose, onSubmit}) => {
+        const [amount, setAmount] = useState('');
+        const [category, setCategory] = useState('');
+
+    }
+
+  return (
+    <div>Expense_Input_Mode</div>
+  )
+}
