fb274ace1f95f2c5a9eb70e97e79a42cb7afe9f7
drypuddle1220 <steventaehyunkim@gmail.com>
2024-10-20 09:58:58 -0400

    input button
    made changes for input button, trying to get the popup input card up.

--- src/components/InputExpense/InputButton.tsx
+++ src/components/InputExpense/InputButton.tsx
@@ -37,7 +37,7 @@ export default function InputButton() {
 
             {IncomePage && (
                 <div className = {styles.inputcard}>
-                    hi
+                    <InputCard/>
                 </div>
             )}
         </div>
---
+++ src/components/InputExpense/InputCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Popup from 'reactjs-popup';
+import styles from './inputCard.module.css';
+
+export default function InputCard() {
+    return (
+        <div>
+            <h4>Popup - GeeksforGeeks</h4>
+            <Popup 
+                trigger={<button>Click to open modal</button>} 
+                modal 
+                nested
+            >
+                hi
+            </Popup>
+        </div>
+    );
+}
